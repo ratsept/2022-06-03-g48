cce1f1cb8d72b32a3791e9b504242dbc0ceb3bb9
Tiit Rätsep <tiit@wazombi.com>
2022-06-04 14:12:35 +0300

    Notification and closest train

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from geopy import distance
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -17,7 +18,14 @@ def trains():
 @app.route("/closest_train")
 def closest_train():
     args = request.args
-    return args
+    r = json.loads(requests.get('https://elron.ee/map_data.json?nocache=1654285204810').text)
+    min_d = float("inf")
+    #d = distance.distance((args["lat"],args["lon"]),(r["data"][0]["latitude"],r["data"][0]["longitude"])).km
+    for train in r["data"]:
+        d = distance.distance((args["lat"],args["lon"]),(train["latitude"],train["longitude"])).km
+        if d < min_d:
+            min_d = d
+    return str(min_d)
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
